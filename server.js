acde0fd8e3618ddbf0ac22275f3684ed82c1d86a
Jesse Parent <jesseparent@gmail.com>
2020-10-29 13:44:10 -0600

    helpers

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
